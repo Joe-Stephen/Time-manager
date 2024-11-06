6a7dfb0655acfed4d71a9d325a2e2e9813bad481
Joe Stephen <joestephenk10@gmail.com>
2024-11-06 16:52:43 +0530

    Log table set up complete.

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,17 +1,34 @@
 import React from "react";
+import MaterialTable from "../utils/Table";
 
 const Body = () => {
+  const data = [
+    { In: "10:00 AM", Out: "10:30 AM", "In-Time": "00:30" },
+    { In: "11:00 AM", Out: "11:30 AM", "In-Time": "00:30" },
+    { In: "01:00 PM", Out: "01:30 PM", "In-Time": "00:30" },
+  ];
   return (
     <>
-      <div className="type-container bg-emerald-600">
+      <div className="type-container bg-emerald-600 p-2">
         <h1 className="">Punching</h1>
-        <button className="text-md p-2 m-4 bg-yellow-500 rounded-lg hover:bg-red-600">
-          Punch In
+      </div>
+      <div className="h-20 w-60 p-2 m-2 bg-gray-400 rounded-md flex justify-center">
+        <button className="text-md p-2 m-2 bg-yellow-500 rounded-lg hover:bg-red-600">
+          Log In
         </button>
-        <button className="text-md p-2 m-4 bg-yellow-500 rounded-lg hover:bg-red-600">
-          Punch Out
+        <button className="text-md p-2 m-2 bg-yellow-500 rounded-lg hover:bg-red-600">
+          Log Out
         </button>
       </div>
+      <div className="h-20 w-60 p-2 m-2 bg-gray-400 rounded-md flex flex-col justify-center">
+        <h1 className="underline underline-offset-4">Timings</h1>
+        <h2>Logged in:</h2>
+        <h2>Logged out:</h2>
+      </div>
+      <div className="h-20 w-60 p-2 m-2 bg-gray-400 rounded-md flex flex-col justify-center">
+        <h1 className="underline underline-offset-4">Logs</h1>
+      </div>
+      <MaterialTable data={data} />
     </>
   );
 };
---
+++ frontend/src/utils/Table.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+type TableRowData = {
+  [key: string]: string | number | boolean;
+};
+
+interface MaterialTableProps {
+  data: TableRowData[];
+}
+
+const MaterialTable: React.FC<MaterialTableProps> = ({ data }) => {
+  if (!data.length) {
+    return <p>No data available</p>;
+  }
+
+  return (
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            {Object.keys(data[0]).map((key) => (
+              <TableCell key={key}>{key}</TableCell>
+            ))}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row, index) => (
+            <TableRow key={index}>
+              {Object.values(row).map((value, idx) => (
+                <TableCell key={idx}>{value}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default MaterialTable;
