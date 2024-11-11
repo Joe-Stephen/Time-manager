24de91a879ca35df38c9a1877d6de8e052bd9c5f
Joe Stephen <joestephenk10@gmail.com>
2024-11-11 13:38:16 +0530

    Completed in time calculation and displaying in table.

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -3,7 +3,13 @@ import moment from "moment";
 import MaterialTable from "../utils/Table";
 import { calculateEstimatedLogOutTime } from "../utils/helpers";
 import { useDispatch, useSelector } from "react-redux";
-import { resetInitialLogin, setInitialLogin } from "../utils/slices/logSlice";
+import {
+  pushLogInTime,
+  pushLogOutTime,
+  resetInitialLogin,
+  resetLogData,
+  setInitialLogin,
+} from "../utils/slices/logSlice";
 import { toggleLoggedInStatus } from "../utils/slices/appSlice";
 
 const Body = () => {
@@ -18,6 +24,7 @@ const Body = () => {
   const lastLoginDate = useSelector((store: any) => store.log.lastLoginDate);
   const initialLogin = useSelector((store: any) => store.log.initialLogin);
   const isLoggedIn = useSelector((store: any) => store.app.isLoggedIn);
+  const logData = useSelector((store: any) => store.log.logData);
 
   //function to handle the log in
   const logInHandler = () => {
@@ -25,10 +32,12 @@ const Body = () => {
       alert("You are already logged in.");
     } else {
       if (lastLoginDate && !moment().isSame(moment(lastLoginDate), "day")) {
-        dispatch(resetInitialLogin());
-      } else {
+        dispatch(resetLogData());
+        dispatch(setInitialLogin(moment()));
+      } else if (!lastLoginDate) {
         dispatch(setInitialLogin(moment()));
       }
+      dispatch(pushLogInTime(moment()));
       dispatch(toggleLoggedInStatus());
     }
   };
@@ -38,6 +47,7 @@ const Body = () => {
     if (!isLoggedIn) {
       alert("You are already logged out.");
     } else {
+      dispatch(pushLogOutTime(moment()));
       dispatch(toggleLoggedInStatus());
     }
   };
@@ -70,7 +80,7 @@ const Body = () => {
         {lastLoginDate ? (
           <h2>
             Estimated log out time:
-            {moment(lastLoginDate).add("hour", 8).format("LTS").toString()}
+            {moment(lastLoginDate).add(8, "hour").format("LTS").toString()}
           </h2>
         ) : (
           <h2>Estimated log out time:</h2>
@@ -79,7 +89,7 @@ const Body = () => {
       </div>
       <div className="p-2 m-2 bg-gray-400 rounded-md flex flex-col justify-center">
         <h1 className="underline underline-offset-4">Logs</h1>
-        <MaterialTable data={data} />
+        <MaterialTable data={logData} />
       </div>
     </>
   );
--- frontend/src/utils/slices/logSlice.ts
+++ frontend/src/utils/slices/logSlice.ts
@@ -5,12 +5,16 @@ interface LogState {
   initialLogin: boolean;
   lastLoginDate: string | null;
   lastLoginTime: Moment | null;
+  logData: [] | any;
+  totalInTime: number;
 }
 
 const initialState: LogState = {
   initialLogin: false,
   lastLoginDate: null,
   lastLoginTime: null,
+  logData: [],
+  totalInTime: 0,
 };
 
 const logSlice = createSlice({
@@ -21,7 +25,9 @@ const logSlice = createSlice({
       if (!state.lastLoginDate) {
         state.initialLogin = true;
         state.lastLoginDate = action.payload;
-      } else if (!moment(state.lastLoginDate).isSame(moment(), "day")) {
+      } else if (
+        !moment(state.lastLoginDate).isSame(moment(action.payload), "day")
+      ) {
         state.initialLogin = true;
       }
     },
@@ -31,8 +37,36 @@ const logSlice = createSlice({
     setLastLoginTime: (state) => {
       state.lastLoginTime = moment();
     },
+    pushLogInTime: (state, action) => {
+      state.logData.push({ In: action.payload.format("LTS").toString() });
+    },
+    pushLogOutTime: (state, action) => {
+      state.logData[state.logData.length - 1].Out = action.payload
+        .format("LTS")
+        .toString();
+      const inTime = moment(
+        state.logData[state.logData.length - 1].In,
+        "hh:mm:ss A"
+      );
+      const outTime = moment(action.payload);
+      const hourDifference = outTime.diff(inTime, "hours");
+      const minuteDifference = outTime.diff(inTime, "minutes") % 60;
+      const secondDifference = outTime.diff(inTime, "seconds") % 60;
+      state.logData[
+        state.logData.length - 1
+      ].InTime = `${hourDifference}:${minuteDifference}:${secondDifference}`;
+    },
+    resetLogData: (state) => {
+      state.logData = [];
+    },
   },
 });
 
-export const { setInitialLogin, resetInitialLogin } = logSlice.actions;
+export const {
+  setInitialLogin,
+  resetInitialLogin,
+  pushLogInTime,
+  pushLogOutTime,
+  resetLogData,
+} = logSlice.actions;
 export default logSlice.reducer;
